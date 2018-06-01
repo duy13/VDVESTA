b951e617d42e281b13f5587bfd8576ae146b108b
root <root@cyber7698.voduy.com>
2018-06-01 22:16:26 +0700

    duy commit

--- freeram.sh
+++ freeram.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 # echo '*/2 * * * * root bash /root/freeram.sh' >> /etc/crontab
-service vesta status &> /dev/null || service vesta start
+run=`netstat -lntup|grep 8083`; if [ "$run" = "" ]; then service vesta restart; fi
 freemem=$(free -m | awk '/Mem:/{print $4}')
 (( freemem <= 100 )) && service httpd reload
