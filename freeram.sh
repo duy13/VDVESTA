9232ca600c49cc5372e3f2cff266a1e9c7b58f13
root <root@cyber7698.voduy.com>
2018-12-04 13:17:43 +0700

    duy commit

--- freeram.sh
+++ freeram.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 # echo '*/2 * * * * root bash /root/freeram.sh' >> /etc/crontab
 run=`netstat -lntup|grep 8083`; if [ "$run" = "" ]; then service vesta restart; fi
-freemem=$(free -m | awk '/Mem:/{print $4}')
-(( freemem <= 100 )) && service httpd reload
+run=`netstat -lntup|grep 8080`; if [ "$run" = "" ]; then service httpd restart; fi
+run=`netstat -lntup|grep 3306`; if [ "$run" = "" ]; then service mysql restart; fi
+RAMsar=`sar -r  1 1 | grep Average:`
+used=`echo "$RAMsar" | awk '{print $3}'| tr . ' ' | awk '{print $1}'`
+buffer=`echo "$RAMsar" | awk '{print $5}'| tr . ' ' | awk '{print $1}'`
+cached=`echo "$RAMsar" | awk '{print $6}'| tr . ' ' | awk '{print $1}'`
+RAMdangdung=$(((used-buffer-cached)/1024)) ;
+RAMtong=$(free -m | awk '/Mem:/{print $2}') ;
+RAMconlai=$((RAMtong-RAMdangdung));
+(( RAMdangdung <= 100 )) && service httpd restart && service varnish restart && service mysql restart && sh -c "sync; echo 3 > /proc/sys/vm/drop_caches"
